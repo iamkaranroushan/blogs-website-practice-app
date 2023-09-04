6c61333352af74551dd01b0fefb25c007d8462ef
Karan Roushan <karan.122roushan@gmail.com>
2023-09-04 16:16:20 +0530

    added database and connected to index.js

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ require('dotenv').config();
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db');
+
+
+
+
 const app = express();
 const port = 8000 || process.env.PORT;
+
+
+// database connection
+
+connectDB();
+
+
+
+
+
 
 app.use(express.static('public'))
 
