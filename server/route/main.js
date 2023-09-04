de3c7bed49c85225bcf5dcadc3dff5e56bcaf8a2
Karan Roushan <karan.122roushan@gmail.com>
2023-09-04 17:25:11 +0530

    created a schema for blogs and connected to index.ejs and main.js , in main.js - created data for the main route and at index.ejs displaying that data

--- server/route/main.js
+++ server/route/main.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
+const post = require('../models/post')
 
 const local = {
     title: "node.js Blog",
     description :"a blogs app"
 }
-router.get('' , (req, res)=>{
-    res.render('index', {local});
+
+router.get('' , async(req, res)=>{
+
+    try {
+        const data = await post.find();
+        res.render('index', {local, data,});
+    } catch (error) {
+        console.log(error);
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/about' , (req, res)=>{
     res.render('about', {local});
@@ -17,4 +36,51 @@ router.get('/about' , (req, res)=>{
 router.get('/contact' , (req, res)=>{
     res.render('contact', {local});
 })
+// function insertPostData () {
+//   post.insertMany([
+//     {
+//       title: "Building APIs with Node.js",
+//       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+//     },
+//     {
+//       title: "Deployment of Node.js applications",
+//       body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+//     },
+//     {
+//       title: "Authentication and Authorization in Node.js",
+//       body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+//     },
+//     {
+//       title: "Understand how to work with MongoDB and Mongoose",
+//       body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+//     },
+//     {
+//       title: "build real-time, event-driven applications in Node.js",
+//       body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+//     },
+//     {
+//       title: "Discover how to use Express.js",
+//       body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+//     },
+//     {
+//       title: "Asynchronous Programming with Node.js",
+//       body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
+//     },
+//     {
+//       title: "Learn the basics of Node.js and its architecture",
+//       body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+//     },
+//     {
+//       title: "NodeJs Limiting Network Traffic",
+//       body: "Learn how to limit netowrk traffic."
+//     },
+//     {
+//       title: "Learn Morgan - HTTP Request logger for NodeJs",
+//       body: "Learn Morgan."
+//     },
+//   ])
+// }
+
+// insertPostData();
+
 module.exports = router;
