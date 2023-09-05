c59d05ad795a2b3d2eac6168b2a71bfa39106453
Karan Roushan <karan.122roushan@gmail.com>
2023-09-05 14:50:54 +0530

    added pagination and accessed the blogpost from database by id

--- server/route/main.js
+++ server/route/main.js
@@ -2,21 +2,49 @@ const express = require('express');
 const router = express.Router();
 const post = require('../models/post')
 
-const local = {
-    title: "node.js Blog",
-    description :"a blogs app"
-}
+
 
 router.get('' , async(req, res)=>{
 
     try {
-        const data = await post.find();
-        res.render('index', {local, data,});
+        const local = {
+            title: "node.js Blog",
+            description :"a blogs app"
+        }
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await post.aggregate([{ $sort: {createdAt: -1}}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+
+        const count = await post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+        res.render('index', {
+            local,
+            data,
+            current:page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+    
     } catch (error) {
         console.log(error);
     }
 })
 
+
+// router.get('' , async(req, res)=>{
+
+//     try {
+//         const data = await post.find();
+//         res.render('index', {local, data,});
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
 
 
 
@@ -36,6 +64,7 @@ router.get('/about' , (req, res)=>{
 router.get('/contact' , (req, res)=>{
     res.render('contact', {local});
 })
+
 // function insertPostData () {
 //   post.insertMany([
 //     {
