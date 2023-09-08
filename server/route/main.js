16de9340f5b9051ce2527bd157aba6f8141d48a9
Karan Roushan <karan.122roushan@gmail.com>
2023-09-08 14:11:11 +0530

    created search results page and in main.js, created a route for search page and added search functionality into the searchbar and linked all of them

--- server/route/main.js
+++ server/route/main.js
@@ -40,16 +40,27 @@ router.post('/search', async(req, res)=>{
 
     try {
 
-        // const local = {
-        //     title: search,
-        //     description :"a blogs app"
-        // }
+        const local = {
+            title: "search",
+            description :"a blogs app"
+        }
 
         let searchTerm = req.body.searchTerm;
 
-        console.log(searchTerm)
-        // const data = await post.find();
-        res.send(searchTerm);
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+        const data = await post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i')}},
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
+
+        
+        res.render("search",{
+            data, 
+            local
+        });
     } catch (error) {
         console.log(error);
     }
